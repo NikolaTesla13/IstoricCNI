442fadd4d254f7b583c9ea66d8e10164db276e24
unknown <asandei.stefanel@gmail.com>
2021-01-22 11:30:05 +0200

    first commit

---
+++ .routify/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "pages": "src/pages",
+  "sourceDir": "public",
+  "routifyDir": ".routify",
+  "ignore": "",
+  "dynamicImports": true,
+  "singleBuild": false,
+  "noHashScroll": false,
+  "distDir": "dist",
+  "hashScroll": true,
+  "extensions": [
+    "svelte",
+    "html",
+    "svx",
+    "md"
+  ],
+  "started": "2021-01-22T08:18:12.987Z"
+}
---
+++ .routify/routes.js
@@ -0,0 +1,127 @@
+
+/**
+ * @roxi/routify 2.8.5
+ * File generated Fri Jan 22 2021 11:04:31 GMT+0200 (Eastern European Standard Time)
+ */
+
+export const __version = "2.8.5"
+export const __timestamp = "2021-01-22T09:04:31.688Z"
+
+//buildRoutes
+import { buildClientTree } from "@roxi/routify/runtime/buildRoutes"
+
+//imports
+
+
+//options
+export const options = {}
+
+//tree
+export const _tree = {
+  "name": "_layout",
+  "filepath": "/_layout.svelte",
+  "root": true,
+  "ownMeta": {
+    "preload": "proximity"
+  },
+  "absolutePath": "D:/cni/cni/src/pages/_layout.svelte",
+  "children": [
+    {
+      "isFile": true,
+      "isDir": false,
+      "file": "_fallback.svelte",
+      "filepath": "/_fallback.svelte",
+      "name": "_fallback",
+      "ext": "svelte",
+      "badExt": false,
+      "absolutePath": "D:/cni/cni/src/pages/_fallback.svelte",
+      "importPath": "../src/pages/_fallback.svelte",
+      "isLayout": false,
+      "isReset": false,
+      "isIndex": false,
+      "isFallback": true,
+      "isPage": false,
+      "ownMeta": {},
+      "meta": {
+        "recursive": true,
+        "preload": "proximity",
+        "prerender": true
+      },
+      "path": "/_fallback",
+      "id": "__fallback",
+      "component": () => import('../src/pages/_fallback.svelte').then(m => m.default)
+    },
+    {
+      "isFile": true,
+      "isDir": false,
+      "file": "index.svelte",
+      "filepath": "/index.svelte",
+      "name": "index",
+      "ext": "svelte",
+      "badExt": false,
+      "absolutePath": "D:/cni/cni/src/pages/index.svelte",
+      "importPath": "../src/pages/index.svelte",
+      "isLayout": false,
+      "isReset": false,
+      "isIndex": true,
+      "isFallback": false,
+      "isPage": true,
+      "ownMeta": {},
+      "meta": {
+        "recursive": true,
+        "preload": "proximity",
+        "prerender": true
+      },
+      "path": "/index",
+      "id": "_index",
+      "component": () => import('../src/pages/index.svelte').then(m => m.default)
+    },
+    {
+      "isFile": true,
+      "isDir": false,
+      "file": "rickroll.svelte",
+      "filepath": "/rickroll.svelte",
+      "name": "rickroll",
+      "ext": "svelte",
+      "badExt": false,
+      "absolutePath": "D:/cni/cni/src/pages/rickroll.svelte",
+      "importPath": "../src/pages/rickroll.svelte",
+      "isLayout": false,
+      "isReset": false,
+      "isIndex": false,
+      "isFallback": false,
+      "isPage": true,
+      "ownMeta": {},
+      "meta": {
+        "recursive": true,
+        "preload": "proximity",
+        "prerender": true
+      },
+      "path": "/rickroll",
+      "id": "_rickroll",
+      "component": () => import('../src/pages/rickroll.svelte').then(m => m.default)
+    }
+  ],
+  "isLayout": true,
+  "isReset": false,
+  "isIndex": false,
+  "isFallback": false,
+  "isPage": false,
+  "isFile": true,
+  "file": "_layout.svelte",
+  "ext": "svelte",
+  "badExt": false,
+  "importPath": "../src/pages/_layout.svelte",
+  "meta": {
+    "preload": "proximity",
+    "recursive": true,
+    "prerender": true
+  },
+  "path": "/",
+  "id": "__layout",
+  "component": () => import('../src/pages/_layout.svelte').then(m => m.default)
+}
+
+
+export const {tree, routes} = buildClientTree(_tree)
+
